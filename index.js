c91b010e2d32a9cabe782c3992c72bfcabe7c0d9
为正 <1033460361@qq.com>
2021-12-08 23:59:59 +0800

    z-on实现，z-model实现,值的响应式实现，下一步需要实现表达式的响应式的方法

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// "use strict"
 // 观察者 模式--收集依赖
 class depend {
   constructor() {
@@ -20,49 +21,83 @@ class depend {
     }
   }
 }
-// 设置命名空间-根据z-data来设置
+// 设置命名空间-根据z-data来设置  支持 z-data='getdata()'  或者z-data='getdata'
 function setNameSpace (namespace) {
   let el_list = document.querySelectorAll('[z-data]')
   el_list.forEach((item, index) => {
     let el_fun = item.getAttribute('z-data')
-    let item_space = eval(el_fun)
+    el_fun = el_fun.replace(/\s/g, '')
+    let item_space
+    // 区分是否带括号
+    if (/\(.*?\)/.test(el_fun)) {
+      item_space = fastEval(el_fun, item)
+    } else {
+      item_space = fastEval(el_fun + '()', item)
+    }
     item.$depend = new depend()
-    getAttr(item_space, item)
     item_space.$refs = {}
-    setRefs(item, item_space)
-
-    return
     let item_space_proxy = new Proxy(item_space, {
       set (target, key, value) {
-        if (key === 'text') {
-          item.$depend.change(key, value)
-        }
+        item.$depend.change(key, value)
         return Reflect.set(target, key, value)
       }
     })
-
-    get_z_show(item_space_proxy, item)
-    get_z_if(item_space_proxy, item)
-    get_z_for(item_space_proxy, item)
-    get_z_model(item_space_proxy, item)
-    get_z_on(item_space_proxy, item)
-    get_z_text(item_space_proxy, item)
-    item_space._name ? item_space._name : (item_space._name = 'namespace' + index)
-    item_space.$el = item
-    item.setAttribute('name', item_space._name)
-    namespace.set(item_space._name, item_space)
+    item_space_proxy._name ? item_space_proxy._name : (item_space_proxy._name = 'zw-namespace' + index)
+    item_space_proxy.$el = item
+    item.setAttribute(item_space_proxy._name, '')
+    // 添加 当前命名空间到namespace
+    namespace.set(item_space_proxy._name, item_space_proxy)
+    get_child(item_space_proxy, item)
   })
 }
+// 递归选任子节点
+function get_child (name_space, el) {
+  //需要考虑当前是否是隐藏和未渲染z-show=false/z-if=false
+  if (!el) { return }
+  render_child(name_space, el)
+  let length = el.children.length
+  if (length > 0) {
+    for (let i = 0; i < length; i++) {
+      get_child(name_space, el.children[i])
+    }
+  }
 
+}
+function render_child (name_space, el) {
 
+  el.setAttribute(name_space._name, '')
+
+  //设置this.$refs
+  setRefs(el, name_space)
+  // 设置 根据z-属性操作
+  getAttr(name_space, el)
+}
+// 比较快速的eval方法
+function fastEval (expression, el) {
+  try {
+    let result = new Function(`return ${expression}`)(expression)
+    return result
+  } catch (e) {
+    warn(e.message, el)
+  }
+}
+//传入上下文 执行eval, 可以传入表达式
+function fastEvalWith (expression, namespace = {}, params = {}, el) {
+  try {
+    let result = new Function(['namespace', ...Object.keys(params)], `let result;with(namespace){result = ${expression}  } return result `)(namespace, ...Object.values(params))
+    return result
+  } catch (e) {
+    warn(e.message, el)
+  }
+}
 const attr_reg = /^z-(on|bind|show|if|model|text|for)/
 // 遍历获取所有z-的属性
 function getAttr (name_space, el) {
   for (let i = 0; i < el.attributes.length; i++) {
     let { name, value } = el.attributes[i]
-    //去除空白符
-    name = name.replace(/\s/g, '')
-    value = value.replace(/\s/g, '')
+    //去除多余空白符
+    name = name.replace(/\s{2,}/g, ' ')
+    value = value.replace(/\s{2,}/g, ' ')
     //替换掉 @ 为z-on  :为z-bind
     if (name[0] === '@') {
       name = name.replace(/^@/, 'z-on')
@@ -110,91 +145,97 @@ function handler_attr (name, value, el, name_space) {
   }
 }
 
+// 已实现
 // 执行绑定z-on: 可以简写为@,可以传递事件对象event，用$event获取（必须在函数中）
 // 如果传递的时候不设置参数的话，默认传递事件对象
 function set_z_on (name, value, el, name_space) {
-  name = name.replace('z-on', '')
+  name = name.replace(attr_reg, '')
   let fun = value.split('(')[0]
   el.addEventListener(name, $event => {
     if (fun === value) {
       // 绑定的形式类似于 z-on:click='show'
-      name_space[fun]($event)
-    } else if (/\(\)/.test(value)) {
-      // 绑定的形式类似于 z-on:click='show()'
-      name_space[fun]($event)
+      fastEvalWith(value + '()', name_space, { $event }, el)
     } else {
       //绑定的形式类似于 z-on:click='show(name,age)'
-      with (name_space) {
-        console.log(value);
-        eval(value)
-      }
+      //或者  z-on:click='show()'
+      fastEvalWith(value, name_space, { $event }, el)
     }
   })
 }
 // 绑定z-bind 可以简写为: 
 function set_z_bind (name, value, el, name_space) {
-  name = name.slice(6)
-  el.setAttribute(name, name_space[value])
+  name = name.replace(attr_reg, '')
+  let attr = fastEvalWith(value, name_space, {}, el)
+  el.setAttribute(name, attr)
 }
 // 绑定z-show
 function set_z_show (name, value, el, name_space) {
-  with (name_space) {
-    if (!eval(value)) {
-      el.style.display = 'none'
-    } else {
-      el.style.removeProperty('display')
-    }
+  let show = fastEvalWith(value, name_space, {}, el)
+  if (!show) {
+    el.style.display = 'none'
+  } else {
+    el.style.removeProperty('display')
   }
 }
 
-// 绑定z-if
+// 绑定z-if  ,有问题,如果由true变成fasle的话，没法显示
 function set_z_if (name, value, el, name_space) {
-  with (name_space) {
-    if (!eval(value)) {
-      el_list[i].parentNode.removeChild(el_list[i])
-    } else {
-      el.style.removeProperty('display')
-    }
+  let show = fastEvalWith(value, name_space, {}, el)
+  if (!show) {
+    el.parentNode.removeChild(el)
+  } else {
+    el.style.removeProperty('display')
   }
-
 }
-// 绑定z-model
+
+// 已实现
+// 双向 绑定z-model，只支持input  且type=text|textarea
 function set_z_model (name, value, el, name_space) {
   if (!(el instanceof HTMLInputElement)) {
     warn('z-model需要绑定在input元素上', el)
   } else {
-    let z_model = el.getAttribute('z-model')
     //设置input的初始化值
-    let value = name_space[z_model]
-    el.value = value
+    el.value = name_space[value]
+    let depend = name_space.$el.$depend
+    //根据namespace中的值变化
+    depend.add(value, (value) => {
+      el.value = value
+    })
+    // 根据input的值，修改namespace中的值
+    el.addEventListener('input', (e) => {
+      name_space[value] = e.target.value
+    })
   }
 }
+
 // 绑定z-text
 function set_z_text (name, value, el, name_space) {
-  el.innerText = name_space[value]
+  let text = fastEvalWith(value, name_space, {}, el)
+  el.innerHTML = text
+  // let depend = name_space.$el.$depend
+  // el.innerText = name_space[value]
+  // depend.add(value, (value) => {
+  //   el.innerText = value
+  // })
+
 }
-// 设置v-for   目前仅仅处理最简单的 item in list
-// 并且只支持一维简单数组，不支持对象数组
+
+// 存在问题，item没有传入
+// 设置v-for  目前仅支持v-for='item in list'这种形式
 function set_z_for (name, value, el, name_space) {
-  let el_list = el.querySelectorAll('[z-for]')
-  for (let i = 0; i < el_list.length; i++) {
-    // 暂时不判断z-key
-    // let key = el_list[i].getAttribute('z-key')
-    // if (!key) {
-    //   warn('请设置z-key', el_list[i])
-    // }
-    let el = el_list[i]
-    let z_for = el_list[i].getAttribute('z-for').trim()
-    let el_html = ''
-    let innerHTML = el_list[i].innerHTML
-    let z_for_list = z_for.split(' ')
-    let list = name_space[z_for_list[2]]
-    for (let index in list) {
-      let html = innerHTML.replace(`{{${z_for_list[0]}}}`, list[index])
-      el_html += html
-    }
-    el.innerHTML = el_html
+  value = value.replace(/\s{2,}/g, ' ').trim()
+  let value_list = value.split(' ')
+  if (value_list.length !== 3 || value_list[1] != 'in') {
+    return warn('z-for中请使用正确的语法 类似于 z-for="item in list"', el)
+  } else if (!(name_space[value_list[2]] instanceof Array)) {
+    return warn('z-for要循环的是数组，请传递数组',el)
   }
+  let div2 = document.createElement('div')
+  let html = el.innerHTML
+  name_space['list'].forEach((item, index) => {
+    div2.innerHTML += html
+  })
+  el.innerHTML = div2.innerHTML
 }
 
 // 添加ref
@@ -206,13 +247,33 @@ function setRefs (el, name_space) {
   }
 }
 // 错误的打印
-function warn (msg, ...agrs) {
-  console.warn('zwapp:' + msg)
-  agrs.forEach(item => {
-    console.warn(item)
+function warn (msg, ...args) {
+  let str = `zwapp expression Error: ${msg}`
+  let el_list = []
+  args.forEach(item => {
+    if (item instanceof HTMLElement) {
+      el_list.push('\n\nError-Element')
+      el_list.push(item)
+    } else {
+      str += '\n\n' + item
+    }
   })
+  console.warn(str, ...el_list);
+  throw new Error(str)
 }
 
+//深拷贝
+function deepClone (obj) {
+  if (typeof obj !== 'object') {
+    return obj
+  }
+  let tempobj;
+  obj instanceof Array ? tempobj = [] : tempobj = {}
+  for (let i in obj) {
+    tempobj[i] = deepClone(obj[i])
+  }
+  return tempobj
+}
 // 初始化命名空间容器
 window.onload = function () {
   let namespace = new Map()
